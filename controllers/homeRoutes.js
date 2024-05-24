bef95c22a234ce7ee190ca320cb1cf94e42e5312
Salokin <nicholas.dpoul@gmail.com>
2024-05-23 20:31:02 -0400

    going to dashboard gives the option to create new post; clicking that takes user to new post page, asking for title and content

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -63,4 +63,12 @@ router.get('/signup', (req, res) => {
 
   res.render('signup');
 });
+
+router.get('/newPost', withAuth, (req, res) => {
+
+  res.render('newPost', {
+  logged_in: req.session.logged_in,
+});
+});
+
 module.exports = router;
